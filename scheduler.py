114caa8946dd4653e912af1badd5e4fdf446ec78
machhindranath70 <131949567+machhindranath70@users.noreply.github.com>
2025-04-09 20:02:00 +0530

    fix: remove Procfile and set Railway config

--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,4 @@
+import os
 from model import (
     clear_timesheets,
     send_email_to_hr,
@@ -5,8 +6,19 @@ from model import (
     send_scheduled_message
 )
 
-# Choose one of these functions depending on the cron job
-clear_timesheets()
-send_email_to_hr("machindranath70@email.com")
-check_and_remind_users()
-send_scheduled_message()
+task = os.getenv("TASK")
+
+if task == "clear":
+    clear_timesheets()
+
+elif task == "email":
+    send_email_to_hr("machindranath70@gmail.com")
+
+elif task == "reminder":
+    check_and_remind_users()
+
+elif task == "scheduled_message":
+    send_scheduled_message()
+
+else:
+    print("⚠️ No valid task specified.")
